fd5832a24273f6fcd97a466010f122a72b44ebfa
nishaya <nishaya@users.noreply.github.com>
2017-12-11 11:48:16 +0900

    add slider

--- box.jsx
+++ box.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import {Button} from '@shopify/polaris';
+import {Button} from '@shopify/polaris'
+import Slider, { Range } from 'rc-slider'
+import 'rc-slider/assets/index.css'
+
+const { Handle } = Slider
 
 const Box = (props) => {
   return (<div>
     <Button>PUSH</Button>
+    <div style={{ margin: 20 }}>
+      <Slider />
+      <Range />
+    </div>
   </div>)
 }
 
