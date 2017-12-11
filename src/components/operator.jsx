4f7176283ca7b56958767b0a628b6222fedb469e
nishaya <nishaya@users.noreply.github.com>
2017-12-11 14:34:30 +0900

    param range

--- src/components/operator.jsx
+++ src/components/operator.jsx
@@ -1,10 +1,12 @@
 // @flow
 
 import React from 'react'
-import {Button} from '@shopify/polaris'
+import { Button, Heading } from '@shopify/polaris'
 import Slider, { Range } from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import type { Props } from './types'
+
+
 
 const Slide = Slider.createSliderWithTooltip(Slider)
 const LabbeledSlider = (props: { label: string, children: any }) => {
@@ -32,17 +34,18 @@ export default class OperatorComponent extends React.Component<Props> {
           <Slide min={0.1} max={16} step={0.1} />
         </LabbeledSlider>
         <div>
+          <Heading>ADSR</Heading>
           <LabbeledSlider label="attack">
-            <Slide min={0.1} max={16} step={0.1} />
+            <Slide min={0.001} max={3} step={0.01} />
           </LabbeledSlider>
           <LabbeledSlider label="decay">
-            <Slide min={0.1} max={16} step={0.1} />
+            <Slide min={0} max={10} step={0.1} />
           </LabbeledSlider>
           <LabbeledSlider label="sustain">
-            <Slide min={0.1} max={16} step={0.1} />
+            <Slide min={0} max={1.0} step={0.01} />
           </LabbeledSlider>
           <LabbeledSlider label="release">
-            <Slide min={0.1} max={16} step={0.1} />
+            <Slide min={0.001} max={10} step={0.1} />
           </LabbeledSlider>
         </div>
       </div>
