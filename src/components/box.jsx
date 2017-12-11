17fee903ab33bd98248b53dd9b3dd135b583d1f4
nishaya <nishaya@users.noreply.github.com>
2017-12-11 12:08:45 +0900

    flowtype

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,12 +1,16 @@
+// @flow
+
 import React from 'react'
 import {Button} from '@shopify/polaris'
 import Slider, { Range } from 'rc-slider'
 import 'rc-slider/assets/index.css'
+import type { Props } from './types'
 
 const { Handle } = Slider
 
-const Box = (props) => {
+const Box = (props: Props) => {
   return (<div>
+    {props.message}
     <Button>PUSH</Button>
     <div style={{ margin: 20 }}>
       <Slider />
