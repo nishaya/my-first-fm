d42743572e896785a8c98589d096c655fce2a130
nishaya <nishaya@users.noreply.github.com>
2017-12-11 16:53:48 +0900

    fm synth

--- src/application.jsx
+++ src/application.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
-import OperatorComponent from './components/operator'
+import OperatorComponent from 'components/operator'
+import { activateKeyEvent, addKeyEvent } from 'utils/browser'
+import Synth from 'fm/synth'
+
+activateKeyEvent()
+const synth = new Synth()
+
+const keys = [
+  'a', 'w', 's', 'e', 'd', 'f',
+  't', 'g', 'y', 'h', 'u', 'j',
+]
+
+keys.forEach((key, i) => {
+  addKeyEvent(key, () => {
+    console.log('play')
+    synth.play(0.5, 440 * (2 ** ((i + 12 - 21)/12)))
+  })
+})
 
 render(
   <OperatorComponent message="Hello React component!!" />,
