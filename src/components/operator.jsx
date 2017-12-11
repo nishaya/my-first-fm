b2f90f0fcfce6dd47ba13a85f800774cbe4f6a92
nishaya <nishaya@users.noreply.github.com>
2017-12-11 12:58:25 +0900

    sliders for parameters

--- src/components/operator.jsx
+++ src/components/operator.jsx
@@ -6,17 +6,46 @@ import Slider, { Range } from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import type { Props } from './types'
 
-const { Handle } = Slider
-
-const OperatorComponent = (props: Props) => {
-  return (<div>
-    {props.message}
-    <Button>PUSH</Button>
-    <div style={{ margin: 20 }}>
-      <Slider />
-      <Range />
+const Slide = Slider.createSliderWithTooltip(Slider)
+const LabbeledSlider = (props: { label: string, children: any }) => {
+  const { label, children } = props
+  return (
+    <div>
+      <div>
+        {label}
+      </div>
+      <div>
+        {children}
+      </div>
     </div>
-  </div>)
+  )
 }
 
-export default OperatorComponent
+export default class OperatorComponent extends React.Component<Props> {
+  render() {
+    const { message } = this.props
+    return (<div>
+      {message}
+      <Button>PUSH</Button>
+      <div style={{ margin: 20 }}>
+        <LabbeledSlider label="freq. ratio">
+          <Slide min={0.1} max={16} step={0.1} />
+        </LabbeledSlider>
+        <div>
+          <LabbeledSlider label="attack">
+            <Slide min={0.1} max={16} step={0.1} />
+          </LabbeledSlider>
+          <LabbeledSlider label="decay">
+            <Slide min={0.1} max={16} step={0.1} />
+          </LabbeledSlider>
+          <LabbeledSlider label="sustain">
+            <Slide min={0.1} max={16} step={0.1} />
+          </LabbeledSlider>
+          <LabbeledSlider label="release">
+            <Slide min={0.1} max={16} step={0.1} />
+          </LabbeledSlider>
+        </div>
+      </div>
+    </div>)
+  }
+}
