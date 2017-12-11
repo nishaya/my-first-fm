1016a76d52190ef978028f84424ebfc720a8405e
nishaya <nishaya@users.noreply.github.com>
2017-12-11 15:32:42 +0900

    eslint

--- src/components/operator.jsx
+++ src/components/operator.jsx
@@ -2,11 +2,9 @@
 
 import React from 'react'
 import { Button, Heading } from '@shopify/polaris'
-import Slider, { Range } from 'rc-slider'
+import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import type { Props } from './types'
-
-
 
 const Slide = Slider.createSliderWithTooltip(Slider)
 const LabbeledSlider = (props: { label: string, children: any }) => {
@@ -26,29 +24,30 @@ const LabbeledSlider = (props: { label: string, children: any }) => {
 export default class OperatorComponent extends React.Component<Props> {
   render() {
     const { message } = this.props
-    return (<div>
-      {message}
-      <Button>PUSH</Button>
-      <div style={{ margin: 20 }}>
-        <LabbeledSlider label="freq. ratio">
-          <Slide min={0.1} max={16} step={0.1} />
-        </LabbeledSlider>
-        <div>
-          <Heading>ADSR</Heading>
-          <LabbeledSlider label="attack">
-            <Slide min={0.001} max={3} step={0.01} />
+    return (
+      <div>
+        {message}
+        <Button>PUSH</Button>
+        <div style={{ margin: 20 }}>
+          <LabbeledSlider label="freq. ratio">
+            <Slide min={0.1} max={16} step={0.1} />
           </LabbeledSlider>
-          <LabbeledSlider label="decay">
-            <Slide min={0} max={10} step={0.1} />
-          </LabbeledSlider>
-          <LabbeledSlider label="sustain">
-            <Slide min={0} max={1.0} step={0.01} />
-          </LabbeledSlider>
-          <LabbeledSlider label="release">
-            <Slide min={0.001} max={10} step={0.1} />
-          </LabbeledSlider>
+          <div>
+            <Heading>ADSR</Heading>
+            <LabbeledSlider label="attack">
+              <Slide min={0.001} max={3} step={0.01} />
+            </LabbeledSlider>
+            <LabbeledSlider label="decay">
+              <Slide min={0} max={10} step={0.1} />
+            </LabbeledSlider>
+            <LabbeledSlider label="sustain">
+              <Slide min={0} max={1.0} step={0.01} />
+            </LabbeledSlider>
+            <LabbeledSlider label="release">
+              <Slide min={0.001} max={10} step={0.1} />
+            </LabbeledSlider>
+          </div>
         </div>
-      </div>
-    </div>)
+      </div>)
   }
 }
