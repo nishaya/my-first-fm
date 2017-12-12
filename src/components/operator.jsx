f3fd908d9e08333a411a4fc1ad72e81ac3d72809
nishaya <nishaya@users.noreply.github.com>
2017-12-12 12:24:30 +0900

    change params

--- src/components/operator.jsx
+++ src/components/operator.jsx
@@ -10,6 +10,7 @@ import { defaultOperatorParams } from 'fm/operator'
 type Props = {
   name: string,
   params: OperatorParams,
+  onChangeParams: (params: OperatorParams) => any,
 }
 
 type State = {
@@ -35,10 +36,37 @@ export default class OperatorComponent extends React.Component<Props, State> {
   static defaultProps = {
     name: '(no name)',
     params: defaultOperatorParams,
+    onChangeParams: (params: OperatorParams) => { console.log(params) },
   }
 
   state: State = {
     params: defaultOperatorParams,
+  }
+
+  componentWillMount() {
+    const { params } = this.props
+    this.setState({ params })
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    const { params } = nextProps
+    if (this.props.params !== params) {
+      this.setState({ params })
+    }
+  }
+
+  changeParam(value: number, key: string) {
+    const { params } = this.state
+    const { adsr } = params
+    const newParams = { ...params, adsr: { ...adsr } }
+    const matched = /^adsr.(.+)$/.exec(key)
+    if (matched) {
+      newParams.adsr[matched[1]] = value
+    } else {
+      newParams[key] = value
+    }
+    this.setState({ params: newParams })
+    this.props.onChangeParams(newParams)
   }
 
   render() {
@@ -51,24 +79,60 @@ export default class OperatorComponent extends React.Component<Props, State> {
         <Card title={`${name}`}>
           <div style={{ margin: 24 }}>
             <LabbeledSlider label="freq. ratio">
-              <Slide min={0.1} max={16} step={0.1} defaultValue={params.freqRatio} />
+              <Slide
+                min={0.1}
+                max={16}
+                step={0.1}
+                defaultValue={params.freqRatio}
+                onChange={(value) => { this.changeParam(value, 'freqRatio') }}
+              />
             </LabbeledSlider>
             <LabbeledSlider label="level">
-              <Slide min={0} max={3.0} step={0.05} defaultValue={params.level} />
+              <Slide
+                min={0}
+                max={3.0}
+                step={0.05}
+                value={params.level}
+                onChange={(value) => { this.changeParam(value, 'level') }}
+              />
             </LabbeledSlider>
             <div>
               <Heading>ADSR</Heading>
               <LabbeledSlider label="attack time">
-                <Slide min={0.001} max={3} step={0.01} defaultValue={adsr.attack} />
+                <Slide
+                  min={0.001}
+                  max={3}
+                  step={0.01}
+                  value={adsr.attack}
+                  onChange={(value) => { this.changeParam(value, 'adsr.attack') }}
+                />
               </LabbeledSlider>
               <LabbeledSlider label="decay time">
-                <Slide min={0} max={10} step={0.1} defaultValue={adsr.decay} />
+                <Slide
+                  min={0}
+                  max={10}
+                  step={0.1}
+                  value={adsr.decay}
+                  onChange={(value) => { this.changeParam(value, 'adsr.decay') }}
+                />
               </LabbeledSlider>
               <LabbeledSlider label="sustain level">
-                <Slide min={0} max={1.0} step={0.01} defaultValue={adsr.sustain} />
+                <Slide
+                  min={0}
+                  max={1.0}
+                  step={0.01}
+                  value={adsr.sustain}
+                  onChange={(value) => { this.changeParam(value, 'adsr.sustain') }}
+                />
               </LabbeledSlider>
               <LabbeledSlider label="release time">
-                <Slide min={0.001} max={10} step={0.1} defaultValue={adsr.release} />
+                <Slide
+                  min={0.001}
+                  max={10}
+                  step={0.1}
+                  value={adsr.release}
+                  onChange={(value) => { this.changeParam(value, 'adsr.release') }}
+                />
               </LabbeledSlider>
             </div>
           </div>
