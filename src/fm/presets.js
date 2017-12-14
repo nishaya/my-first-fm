f0ae371ab3db8880320c3b129910477279afe1be
nishaya <nishaya@users.noreply.github.com>
2017-12-14 16:02:13 +0900

    add organ

--- src/fm/presets.js
+++ src/fm/presets.js
@@ -3,9 +3,11 @@
 import electricPiano from './presets/electric_piano'
 import synthBass from './presets/synth_bass'
 import toyPiano from './presets/toy_piano'
+import organ from './presets/organ'
 
 const presets = [
   electricPiano,
+  organ,
   synthBass,
   toyPiano,
 ]
---
+++ src/fm/presets/organ.js
@@ -0,0 +1,65 @@
+/* eslint import/prefer-default-export: 0 */
+
+const preset = {
+  name: 'Organ (#12)',
+  algo: [
+    {
+      dest: -1,
+      type: 'carrier',
+      params: {
+        freqRatio: 1.0,
+        level: 0.8,
+        adsr: {
+          attack: 0.001,
+          decay: 0,
+          sustain: 1,
+          release: 0.1,
+        },
+      },
+    },
+    {
+      dest: -1,
+      type: 'carrier',
+      params: {
+        freqRatio: 0.5001,
+        level: 0.8,
+        adsr: {
+          attack: 0.001,
+          decay: 0,
+          sustain: 1,
+          release: 0.1,
+        },
+      },
+    },
+    {
+      dest: -1,
+      type: 'carrier',
+      params: {
+        freqRatio: 6.2,
+        level: 0.8,
+        adsr: {
+          attack: 0.001,
+          decay: 0,
+          sustain: 1,
+          release: 0.1,
+        },
+      },
+    },
+    {
+      dest: -1,
+      type: 'carrier',
+      params: {
+        freqRatio: 8.1,
+        level: 0.8,
+        adsr: {
+          attack: 0.001,
+          decay: 0,
+          sustain: 1,
+          release: 0.1,
+        },
+      },
+    },
+  ],
+}
+
+export default preset
